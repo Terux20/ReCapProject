87feb26aea8dca3fb72c909e2f90a017e8efff0f
Terux20 <155570356+Terux20@users.noreply.github.com>
2024-01-26 13:47:33 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarService carService = new CarManager(new EfCarDal());
+            
+            var result = carService.GetAll();
+         
+            if (result.Success==true)
+            {
+                foreach (var car in result.Data)
+                {
+                    Console.WriteLine(car.CarName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
+        }
+    }
+}
